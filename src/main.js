cad7df72aa9d449a5ad70fc57a786bc887b8860a
Iryna Lebedeva <irynalebedeva@Irynas-MacBook-Air.local>
2025-11-17 12:02:50 +0200

    commit msg name

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+const burger = document.querySelector('#burger');
+const mobileContainer = document.querySelector('#mobile-container');
+
+burger.addEventListener('click', () => {
+  mobileContainer.classList.toggle('hidden');
+});
